d5c08d14cb229d43db47106394372bfbcd1a0a93
xa0082249956 <576810415@qq.com>
2020-02-15 23:13:55 +0800

    disable some plugins

--- update_all.ps1
+++ update_all.ps1
@@ -56,41 +56,41 @@ $Options = [ordered]@{
         Path = "$PSScriptRoot\Update-History.md"            #Path where to save history
     }
 
-    Gist = @{
-        Id     = $Env:gist_id                               #Your gist id; leave empty for new private or anonymous gist
-        ApiKey = $Env:github_api_key                        #Your github api key - if empty anoymous gist is created
-        Path   = "$PSScriptRoot\Update-AUPackages.md", "$PSScriptRoot\Update-History.md"       #List of files to add to the gist
-    }
+    # Gist = @{
+    #     Id     = $Env:gist_id                               #Your gist id; leave empty for new private or anonymous gist
+    #     ApiKey = $Env:github_api_key                        #Your github api key - if empty anoymous gist is created
+    #     Path   = "$PSScriptRoot\Update-AUPackages.md", "$PSScriptRoot\Update-History.md"       #List of files to add to the gist
+    # }
 
-    Git = @{
-        User     = ''                                       #Git username, leave empty if github api key is used
-        Password = $Env:github_api_key                      #Password if username is not empty, otherwise api key
-    }
+    # Git = @{
+    #     User     = ''                                       #Git username, leave empty if github api key is used
+    #     Password = $Env:github_api_key                      #Password if username is not empty, otherwise api key
+    # }
 
-    GitReleases  = @{
-        ApiToken    = $Env:github_api_key                   #Your github api key
-        ReleaseType = 'package'                             #Either 1 release per date, or 1 release per package
-    }
+    # GitReleases  = @{
+    #     ApiToken    = $Env:github_api_key                   #Your github api key
+    #     ReleaseType = 'package'                             #Either 1 release per date, or 1 release per package
+    # }
 
-    RunInfo = @{
-        Exclude = 'password', 'apikey', 'apitoken'          #Option keys which contain those words will be removed
-        Path    = "$PSScriptRoot\update_info.xml"           #Path where to save the run info
-    }
+    # RunInfo = @{
+    #     Exclude = 'password', 'apikey', 'apitoken'          #Option keys which contain those words will be removed
+    #     Path    = "$PSScriptRoot\update_info.xml"           #Path where to save the run info
+    # }
 
-    Mail = if ($Env:mail_user) {
-            @{
-                To         = $Env:mail_user
-                From       = $Env:mail_from
-                Server     = $Env:mail_server
-                UserName   = $Env:mail_user
-                Password   = $Env:mail_pass
-                Port       = $Env:mail_port
-                EnableSsl  = $Env:mail_enablessl -eq 'true'
-                Attachment = "$PSScriptRoot\update_info.xml"
-                UserMessage = ''
-                SendAlways  = $false                        #Send notifications every time
-             }
-           } else {}
+    # Mail = if ($Env:mail_user) {
+    #         @{
+    #             To         = $Env:mail_user
+    #             From       = $Env:mail_from
+    #             Server     = $Env:mail_server
+    #             UserName   = $Env:mail_user
+    #             Password   = $Env:mail_pass
+    #             Port       = $Env:mail_port
+    #             EnableSsl  = $Env:mail_enablessl -eq 'true'
+    #             Attachment = "$PSScriptRoot\update_info.xml"
+    #             UserMessage = ''
+    #             SendAlways  = $false                        #Send notifications every time
+    #          }
+    #        } else {}
 
     ForcedPackages = $ForcedPackages -split ' '
     BeforeEach = {
@@ -112,4 +112,4 @@ $global:au_GalleryUrl   = ''             #URL to package gallery, leave empty fo
 $global:info = updateall -Name $Name -Options $Options
 
 #Uncomment to fail the build on AppVeyor on any package error
-#if ($global:info.error_count.total) { throw 'Errors during update' }
+if ($global:info.error_count.total) { throw 'Errors during update' }
